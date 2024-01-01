0b469ff87bf03dfa2e6333b2ea5129cee36e8dbb
Md Anas Sabah <mdanassabah@gmail.com>
2024-01-02 00:44:28 +0530

    integrated fabric.js and refactored the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import VideoPlayer from "./components/VideoPlayer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [videoFile, setVideoFile] = useState(null);
+
+  const handleVideoChange = (e) => {
+    const file = e.target.files[0];
+    setVideoFile(URL.createObjectURL(file));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="bg-black h-screen">
+      <input type="file" accept="video/*" onChange={handleVideoChange} />
+      {videoFile && <VideoPlayer videoFile={videoFile} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+import { useRef, useEffect } from "react";
+import { fabric } from "fabric";
+import * as faceapi from "face-api.js";
+
+function VideoPlayer({ videoFile }) {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const initVideo = async () => {
+      const video = videoRef.current;
+      const canvas = new fabric.Canvas(canvasRef.current);
+
+      await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+      await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+      await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+
+      video.addEventListener("play", () => {
+        const displaySize = { width: video.width, height: video.height };
+        faceapi.matchDimensions(canvas.getElement(), displaySize);
+        console.log(displaySize);
+
+        setInterval(async () => {
+          const detections = await faceapi
+            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceDescriptors();
+          const resizedDetections = faceapi.resizeResults(
+            detections,
+            displaySize
+          );
+          canvas.clear();
+
+          resizedDetections.forEach((detection) => {
+            const { x, y, width, height } = detection.detection.box;
+            const rect = new fabric.Rect({
+              left: x,
+              top: y,
+              width,
+              height,
+              fill: "transparent",
+              stroke: "red",
+              strokeWidth: 2,
+            });
+            canvas.add(rect);
+          });
+        }, 100);
+      });
+    };
+
+    initVideo();
+  }, [videoFile]);
+
+  return (
+    <div>
+      <video ref={videoRef} width="1210" height="1080" controls>
+        <source src={videoFile} type="video/mp4" />
+      </video>
+      <canvas ref={canvasRef} width="1210" height="480"></canvas>
+      {/* <canvas ref={canvasRef} width="640" height="480"></canvas> */}
+    </div>
+  );
+}
+
+export default VideoPlayer;
